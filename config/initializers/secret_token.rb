d404d78fcc80d71bfc05f74c5437789a29cc4715
John Kotzi <jkotzi@yahoo.com>
2013-02-13 09:51:03 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mathra::Application.config.secret_token = '3a225ac13de374809fe5b01f8294c164956a372cbb125b033a2d142eacb33b511bed9b6b9af9e899d048ea9774c4ae8caab28817bf1d4fcc31e28fc494f2993b'
